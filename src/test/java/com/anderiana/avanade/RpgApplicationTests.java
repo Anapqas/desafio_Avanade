d8d42670320d5e8d2a47b3e6160f91924c4f6576
Anapqas <ana.anderi@gmail.com>
2023-09-09 10:42:28 -0300

    commit inicial

---
+++ src/test/java/com/anderiana/avanade/RpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderiana.avanade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
